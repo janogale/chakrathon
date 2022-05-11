521734a04349d8bdad0c7e084ec34b21c2915db7
janagale <mukhtar.mahomed@gmail.com>
2022-05-11 16:11:42 +0300

    Minor refactors

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,12 +3,14 @@ import { Code, Flex, Heading } from "@chakra-ui/react";
 export const Hero = () => (
   <Flex
     justifyContent="center"
+    flexDirection={["column", "row"]}
     alignItems="center"
     bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
     bgClip="text"
   >
-    <Heading>
-      Yuki Team <Code>{" <"}Chakrathon Challenge{"/>"}</Code>
-    </Heading>
+    <Heading>Yuki Team</Heading>
+    <Code ml={4} mt={2}>
+      {"<"}Chakrathon Challenge{"/>"}
+    </Code>
   </Flex>
 );
